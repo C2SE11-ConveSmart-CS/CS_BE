c58a30b0a69d3a5b0783bb765c83d03c6c535027
Phạm Đức Giang <ducgiang01@192.168.2.43>
2024-10-23 00:06:37 +0700

    upload models

---
+++ src/app/models/role.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const Role = new Schema({
+  org: {
+    type: Schema.ObjectId,
+    ref: 'organization'
+  },
+  name: {
+    type: String
+  },
+  reportingTo: {
+    type: String
+  },
+  shareWithPeers: {
+    type: Boolean,
+    default: false
+  }
+});
+
+// indexing
+Role.index({
+  org: 1
+});
+
+Role.set('toJSON', { getters: true });
+
+
+module.exports = mongoose.model('role', Role);
