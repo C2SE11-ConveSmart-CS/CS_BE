06dc989c1d7e3a98bd2261a884f7efa128289513
Le-Loc <leeloc1069@gmail.com>
2024-11-05 11:13:56 +0700

    whatsapp

--- src/app.js
+++ src/app.js
@@ -1,39 +1,42 @@
-require('dotenv').config()
-const { app, server } = require('./app/lib/socket.io')
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const authRoutes = require('./app/routes/auth')
-const chatRoutes = require('./app/routes/messenger')
-const PORT = process.env.PORT || 5001
+require("dotenv").config();
+const { app, server } = require("./app/lib/socket.io");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./app/routes/auth");
+const chatRoutes = require("./app/routes/messenger");
+const PORT = process.env.PORT || 5001;
+const messageWhatsappRouters = require("./app/routes/messageWhatsappRouter");
 
 app.use(
   cors({
     origin: [process.env.FRONT_END_URL],
-    credentials: true
+    credentials: true,
   })
-)
-app.use(express.json()) // Xử lý các yêu cầu JSON
-app.use('/api/auth', authRoutes) // Đăng ký các route xác thực
-app.use('/api/chats', chatRoutes)
+);
+app.use(express.json()); // Xử lý các yêu cầu JSON
+app.use("/api/auth", authRoutes); // Đăng ký các route xác thực
+app.use("/api/chats", chatRoutes);
 
 // Kết nối với MongoDB
 mongoose
-  .connect('mongodb://localhost:27017', {
+  .connect("mongodb://localhost:27017", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
-    console.log('Kết nối MongoDB thành công')
+    console.log("Kết nối MongoDB thành công");
   })
   .catch((err) => {
-    console.error('Lỗi kết nối MongoDB:', err)
-  })
+    console.error("Lỗi kết nối MongoDB:", err);
+  });
 
 // Route gốc đơn giản
-app.get('/', (req, res) => {
-  res.send('Chào mừng đến với API!')
-})
+app.get("/", (req, res) => {
+  res.send("Chào mừng đến với API!");
+});
+
+app.use("/api/messages", messageWhatsappRouters);
 
 // Khởi chạy server
-server.listen(PORT, () => console.log(`Server listenning on port ${PORT}...`))
+server.listen(PORT, () => console.log(`Server listenning on port ${PORT}...`));
